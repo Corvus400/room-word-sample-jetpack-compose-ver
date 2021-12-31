79e23e98380e2d6d0ab6cc517012402d62ed414c
todayama_r <13657682+Corvus400@users.noreply.github.com>
2021-12-31 23:15:14 +0900

    :memo: Change NewWordActivityをJetpackCompose化 要リファクタ

--- app/src/main/java/com/example/roomwordsample/presentation/ui/NewWordActivity.kt
+++ app/src/main/java/com/example/roomwordsample/presentation/ui/NewWordActivity.kt
@@ -2,37 +2,111 @@ package com.example.roomwordsample.presentation.ui
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
-import android.widget.Button
-import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.roomwordsample.R
+import com.example.roomwordsample.presentation.ui.theme.RoomWordTheme
 
-class NewWordActivity : AppCompatActivity() {
-
-    private lateinit var editWordView: EditText
+class NewWordActivity : ComponentActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_word)
-        editWordView = findViewById(R.id.edit_word)
-
-        val button = findViewById<Button>(R.id.button_save)
-        button.setOnClickListener {
-            val replyIntent = Intent()
-            if (TextUtils.isEmpty(editWordView.text)) {
-                setResult(Activity.RESULT_CANCELED, replyIntent)
-            } else {
-                val word = editWordView.text.toString()
-                replyIntent.putExtra(EXTRA_REPLY, word)
-                setResult(Activity.RESULT_OK, replyIntent)
+        setContent {
+            RoomWordTheme {
+                NewWordScreen(
+                    onClickSave = { text ->
+                        val replyIntent = Intent()
+                        if (text.isEmpty()) {
+                            setResult(Activity.RESULT_CANCELED, replyIntent)
+                        } else {
+                            replyIntent.putExtra(EXTRA_REPLY, text)
+                            setResult(Activity.RESULT_OK, replyIntent)
+                        }
+                        finish()
+                    }
+                )
             }
-            finish()
         }
     }
 
     companion object {
         const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
+    }
+
+    @Composable
+    fun NewWordScreen(
+        onClickSave: (String) -> Unit
+    ) {
+        var text by rememberSaveable {
+            mutableStateOf("")
+        }
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = "Register new words"
+                        )
+                    }
+                )
+            },
+        ) {
+            Column {
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .sizeIn(minHeight = 48.dp)
+                        .padding(16.dp),
+                    value = text,
+                    textStyle = TextStyle(fontSize = 18.sp),
+                    label = {
+                        Text(stringResource(R.string.hint_word))
+                    },
+                    onValueChange = {
+                        text = it
+                    }
+                )
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    onClick = {
+                        onClickSave(text)
+                    }
+                ) {
+                    Text(
+                        color = colorResource(R.color.buttonLabel),
+                        text = stringResource(R.string.button_save)
+                    )
+                }
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun NewWordScreenPreview() {
+        RoomWordTheme {
+            NewWordScreen({})
+        }
     }
 }

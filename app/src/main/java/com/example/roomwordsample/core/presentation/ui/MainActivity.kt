18b3c0743a247f3cd6ced2cb6b2716c5f51729f9
todayama_r <13657682+Corvus400@users.noreply.github.com>
2021-12-31 12:08:52 +0900

    :sparkles: Add Codelabに沿ってAdapterを追加

--- app/src/main/java/com/example/roomwordsample/core/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/roomwordsample/core/presentation/ui/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.roomwordsample.core.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.roomwordsample.R
+import com.example.roomwordsample.core.presentation.ui.items.WordListAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }

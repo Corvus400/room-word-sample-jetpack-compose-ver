c7aa17218c8f77312c46a11074da7408fc464fab
todayama_r <13657682+Corvus400@users.noreply.github.com>
2021-12-31 11:46:50 +0900

    :sparkles: Add Codelabに沿ってViewModelを追加

---
+++ app/src/main/java/com/example/roomwordsample/core/presentation/ui/WordViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.roomwordsample.core.presentation.ui
+
+import androidx.lifecycle.*
+import com.example.roomwordsample.core.domain.entity.Word
+import com.example.roomwordsample.core.infrastructure.repository.WordRepository
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+
+
+class WordViewModel(
+    private val repository: WordRepository
+) : ViewModel() {
+    val allWords: LiveData<List<Word>> = repository.allWords.asLiveData()
+
+    fun insert(word: Word) = viewModelScope.launch {
+        repository.insert(word)
+    }
+}
+
+class WordViewModelFactory(private val repository: WordRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return WordViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
